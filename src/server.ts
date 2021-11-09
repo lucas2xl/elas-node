09d2b9fe4e880638093eb8e31c31c4596689ec25
Lucas Aguiar <lucas.aguiar@khomp.com>
2021-11-09 15:42:55 -0300

    configure heroku port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from '@shared/http/app';
 
-const port = process.env.APP_API_PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
