19d6885e00754d75686d7caae67a8f7f0ab48f9a
Lucas Aguiar <lucas.aguiar@khomp.com>
2021-11-09 16:50:45 -0300

    update

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@types': './src/types',
+          '@configs': './src/configs',
+          '@interfaces': './src/interfaces',
+          '@modules': './src/modules',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
