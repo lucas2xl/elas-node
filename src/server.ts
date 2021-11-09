d258535ab7d923fb7a87fcc992ed37776357eb4a
Lucas Aguiar <lucas.aguiar@khomp.com>
2021-11-09 15:50:11 -0300

    update port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from '@shared/http/app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
