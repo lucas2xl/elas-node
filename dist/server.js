4db0ebf19e817d20a72db897c258ce793f2728b4
Lucas Aguiar <lucas.aguiar@khomp.com>
2021-11-10 09:41:52 -0300

    Update heroku

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = require("./shared/http/app");
-
-const port = process.env.PORT || 4000;
-
-_app.app.listen(port, () => {
-  console.log(`🚀 Server is running on port ${port}`);
-});
